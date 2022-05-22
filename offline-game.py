df1d3b06a5ee5af737d7eba7ef8f1be2a34f4ec3
Egor Fursov <egorf@live.ru>
2022-05-22 22:26:36 +0300

    Now program can generate files with exercises

--- offline-game.py
+++ offline-game.py
@@ -1,7 +1,9 @@
 from fnmatch import fnmatch
 import pathlib
-import yaml
 import os
+import pandas as pd
+import numpy as np
+
 
 
 # This function returns YAML from my Obsidian note as one line
@@ -12,7 +14,7 @@ def parse_yaml(lines):
     t_count = 0
 
     for line in lines:
-        if line.strip('\n').strip('\t') == '---':
+        if line.strip('\n\t') == '---':
             t_count += 1
             continue
         t_yaml.append(line)
@@ -25,25 +27,71 @@ def parse_yaml(lines):
     
     return yaml
 
+# This function returns all files mathcing pattern from given root and subdirectories
 def get_filepaths(root, pattern):
     filepaths = list()
 
     for path, subdirs, files in os.walk(root):
         for name in files:
             if fnmatch(name, pattern):
-                filepaths.append(pathlib.PurePath(path, name)) #os.path.join(path, name)) / pathlib.PurePath(path, name)
+                filepaths.append(pathlib.PurePath(path, name))
 
     return filepaths
 
+# This function returns only paths to notes with specified tags
+def get_vokabel_filepaths(directory, tag_pattern):
+    # directory = 'C:\Guts'
+    # tag_pattern = '*flashcards*'
+    
+    filepaths = get_filepaths(directory, '*.md')
+    vokabel_notes = list()
 
-filepaths = get_filepaths('C:\Guts', '*.md')
-tag = '*flashcards*'
+    for path in filepaths:
+        with open(path, 'r', encoding='UTF-8') as stream:
+            lines = stream.readlines()
+            yaml = parse_yaml(lines)
+            if fnmatch(yaml, tag_pattern):
+                vokabel_notes.append(path)
 
-for path in filepaths:
-    with open(path, 'r', encoding='UTF-8') as stream:
-        lines = stream.readlines()
-        yaml = parse_yaml(lines)
-        if fnmatch(yaml, tag):
-            print(path)
+    return vokabel_notes
 
-  
+
+# This function gives list of tuples with translations
+def get_translations_from_note(path_to_note):
+    words = list()
+    pattern = '*:::*'
+
+    with open(path_to_note, 'r', encoding='UTF-8') as stream:
+        for line in stream.readlines():
+            if fnmatch(line, pattern):
+                # Remove all technical characters, than remove spaces
+                t = tuple(map(lambda x: x.strip(), line.strip('\n\t').split(':::')))
+                # Convert to Series
+                row = pd.Series(t, index=['DE', 'EN/RU'])
+                words.append(row)
+    
+    return pd.concat(words, axis=1).T
+
+
+# This function returns exersices split in days
+def create_exersice_dataframes(dataframes, n_days):
+    df = pd.concat(dataframes, axis=0)
+    df = df.sample(frac=1).reset_index(drop=True)
+    days = np.array_split(df, n_days)
+
+    # Write days to files
+    for i in range(len(days)):
+        days[i].to_csv(path_or_buf=f'./Day_{i}.csv', sep='\t')
+
+    return days
+
+
+def generate_exercises(directory, tag, n_days):
+    dataframes = []
+    for path in get_vokabel_filepaths(directory, tag):
+        dataframes.append(get_translations_from_note(path))
+
+    create_exersice_dataframes(dataframes, n_days)
+
+
+generate_exercises('C:\Guts', '*flashcards*', 5)
